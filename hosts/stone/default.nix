346c34c949521baef1ae1550599f9ae4fa940ccf
TwoStone <johannes@halbachnet.de>
2025-06-07 12:02:20 +0100

    Added stone config files

---
+++ hosts/stone/default.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+{
+  imports = [
+    ./hardware-configuration.nix
+    ./../../modules/core
+  ];
+
+  powerManagement.cpuFreqGovernor = "performance";
+  services.blueman.enable = true;
+  #hardware.pulseaudio.enable = true;
+  hardware.bluetooth.enable = true;
+
+  # Enable OpenGL
+  hardware.graphics = {
+    enable = true;
+  };
+}
