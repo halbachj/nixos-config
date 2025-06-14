10be25225fcd42ebe8945350a5f2d9a416ec85b0
Johannes Halbach <johannes@halbachnet.de>
2025-06-15 00:30:09 +0100

    Made explicit sound choices

--- hosts/feather/configuration.nix
+++ hosts/feather/configuration.nix
@@ -20,7 +20,7 @@
     flake.nixosModules.laptop-base
     flake.nixosModules.desktop-sway
 
-    flake.homeModules.desktop-games
+    flake.nixosModules.desktop-games
 
     flake.nixosModules.users-twostone
   ];
---
+++ modules/nixos/desktop-base/sound.nix
@@ -0,0 +1,23 @@
+_: {
+  hardware.pulseaudio.enable = false; # Use Pipewire, the modern sound subsystem
+
+  security.rtkit.enable = true; # Enable RealtimeKit for audio purposes
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # Uncomment the following line if you want to use JACK applications
+    # jack.enable = true;
+  };
+
+  #
+  # Bluetooth
+  #
+  hardware.bluetooth = {
+    enable = true;
+    powerOnBoot = true;
+  };
+}
+
