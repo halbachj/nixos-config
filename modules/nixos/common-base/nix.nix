84a1ffc942ed4196c1d600ba48b6eb0e6b5e93ba
Johannes Halbach <johannes@halbachnet.de>
2025-10-10 17:20:36 +0100

    Removed missing cache server

--- modules/nixos/common-base/nix.nix
+++ modules/nixos/common-base/nix.nix
@@ -58,7 +58,6 @@
       ];
       trusted-public-keys = [
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
       experimental-features = [
